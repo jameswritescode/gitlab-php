a663c279fe1f61682c08268697bd9576b27a61db
James Newton <james@Zaphyous.com>
2013-01-16 21:35:24 -0600

    make Request class work so I can work on everything else

--- gitlab/request.php
+++ gitlab/request.php
@@ -2,24 +2,68 @@
 
 class Request
 {
-    protected function authorize($domain, $token)
+    private $curl;
+    private $domain;
+    private $token;
+
+    public function __construct($domain, $token)
     {
+        $this->domain = $domain;
+        $this->token = $token;
     }
 
-    protected function get($endpoint, $params = array())
+    private function url($endpoint, $params)
     {
+        $params['private_token'] = $this->token;
+
+        $query = '?' . http_build_query($params);
+
+        return "$this->domain$endpoint$query";
     }
 
-    protected function post($endpoint, $params = array())
+    private function request($endpoint, $params, $method)
     {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $this->url($endpoint, $params));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Accept: application/json',
+            'Content-Type: application/json'
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        if ($method == 'POST') {
+            curl_setopt($curl, CURLOPT_POST, TRUE);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        } elseif ($method == 'PUT') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        } elseif ($method == 'DELETE') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        }
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
     }
 
-    protected function patch($endpoint, $params = array())
-    {
+    protected function get($endpoint, $params = array()) {
+        return $this->request($endpoint, $params, 'GET');
     }
 
-    protected function delete($endpoint)
-    {
+    protected function post($endpoint, $params = array()) {
+        return $this->request($endpoint, $params, 'POST');
+    }
+
+    protected function patch($endpoint, $params = array()) {
+        return $this->request($endpoint, $params, 'PUT');
+    }
+
+    protected function delete($endpoint) {
+        return $this->request($endpoint, '', 'DELETE');
     }
 }
 
